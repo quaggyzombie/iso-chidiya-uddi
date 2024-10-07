4625cc8337fda07d8d0df57e8a58c80b71af686e
quaggyzombie <gamerchampionlast@gmail.com>
2024-10-07 18:48:12 +0530

    Again restructured because I didnt like the previous one

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-from app import app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/heartbeat')
+def heartbeat():
+    return "I am up!"
+
+@app.route("/newgame")
+def newgame():
+    return "NEW GAME STARTED"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5004)
