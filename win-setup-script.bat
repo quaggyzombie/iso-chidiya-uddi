555273cc0926815d599709c2e5b3ec311f331aba
quaggyzombie <gamerchampionlast@gmail.com>
2024-10-07 18:09:53 +0530

    win-setup-script.bat now detects project dir and creates a venv, installs dependencies in the venv

--- win-setup-script.bat
+++ win-setup-script.bat
@@ -1,4 +1,7 @@
 rem This script aims to set up the project in a local env on windows.
 rem Sorry, not sorry linux users, do your own thing.
-
+set PROJECT_DIR=%CD%
+if not exist "%PROJECT_DIR%\.venv" (py -3 -m venv .venv)
+call ".venv\Scripts\activate"
 pip3 install -r requirements.txt
+deactivate
